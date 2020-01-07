ef4951e6dbe4da3f5300eaccea4e594f16187a8e
saikumarpuli <saikumarpuli9@gmail.com>
2020-01-07 19:29:53 +0530

    practiced typescrit evolution and mixins

--- Basics/mixins.ts
+++ Basics/mixins.ts
@@ -1,10 +1,10 @@
+/*
 // Disposable Mixin
 class Disposable {
     isDisposed: boolean;
     dispose() {
         this.isDisposed = true;
     }
-
 }
 
 // Activatable Mixin
@@ -45,3 +45,82 @@ function applyMixins(derivedCtor: any, baseCtors: any[]) {
         });
     });
 }
+ */
+
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+function Timestamped<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    timestamp = Date.now();
+  };
+}
+
+class User {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+// Create a new class by mixing `Timestamped` into `User`
+const TimestampedUser = Timestamped(User);
+
+// Instantiate the new `TimestampedUser` class
+const user = new TimestampedUser("Latha Pathuri");
+
+// We can now access properties from both the `User` class
+// and our `Timestamped` mixin in a type-safe manner
+console.log(user.name);
+console.log(user.timestamp);
+console.log(Date.now());
+
+function Tagged<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    tag: string | null;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.tag = null;
+    }
+  };
+}
+// Create a new class by mixing `Tagged` into `User`
+const TaggedUser = Tagged(User);
+
+// Instantiate the new `TaggedUser` class
+const userTagged = new TaggedUser("John Doe");
+
+// We can now assign values to any property defined in either
+// the `User` class or our `Tagged` mixin in a type-safe manner.
+// TypeScript will type-check those assignments!
+userTagged.name = "Rama";
+userTagged.tag = "pathuri";
+
+console.log(userTagged);
+
+function Activatable<TBase extends Constructor>(Base: TBase){
+  return class extends Base{
+    isActivated = false;
+    activate(){
+      this.isActivated = true;
+    }
+    deactivate(){
+      this.isActivated = false;
+    }
+  };
+}
+
+const ActivatableUser = Activatable(User);
+const activableUser = new ActivatableUser("Laxmi");
+
+console.log(activableUser.isActivated);
+console.log( activableUser.activate() );
+console.log(activableUser.isActivated);
+console.log( activableUser.deactivate() );
+console.log(activableUser.isActivated);
+
+const SpecialUser = Activatable(Tagged(Timestamped(User)));
+const userSpecial = new SpecialUser("Loukhya Sri");
+userSpecial.
---
+++ Basics/nonNullable.ts
@@ -0,0 +1,17 @@
+let name: string;
+//name = "Latha";
+//name = null;
+//name = undefined;
+
+let age: number;
+age = 24;
+//age = null;
+//age = undefined;
+
+let isMarried: boolean;
+isMarried = true;
+isMarried = false;
+//isMarried = null;
+//isMarried = undefined;
+
+console.log(name+" "+age+" "+isMarried);
